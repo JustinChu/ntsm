242829cc69835679a530b4463b98d978bcac81db
cjustin <cjustin@cjustin-G14-laptop>
2021-07-13 05:51:15 -0400

    bugfix

--- scripts/parseCounts.pl
+++ scripts/parseCounts.pl
@@ -7,28 +7,29 @@ use strict;
 use diagnostics;
 use IO::File;
 
-my $fh   = new IO::File( $ARGV[0], "r" );
-my $line = $fh->getline();
-
+my $fh        = new IO::File( $ARGV[0], "r" );
+my $line      = $fh->getline();
 my $threshold = 3;
 
 while ($line) {
 	chomp($line);
-	my @tempArr = split( / /, $line );
+	if ($line) {
+		my @tempArr = split( / /, $line );
+#		print $tempArr[1];
 
-	if ( $tempArr[0] > $threshold && $tempArr[2] > $threshold ) {
-		print "3";
+		if ( $tempArr[0] > $threshold && $tempArr[1] > $threshold ) {
+			print "3";	
+		}
+		elsif ( $tempArr[0] > $threshold ) {
+			print "1";
+		}
+		elsif ( $tempArr[1] > $threshold ) {
+			print "2";
+		}
+		else {
+			print "0";
+		}
 	}
-	elsif ( $tempArr[0] > $threshold ) {
-		print "1";
-	}
-	elsif ( $tempArr[1] > $threshold ) {
-		print "2";
-	}
-	else {
-		print "0";
-	}
-
 	$line = $fh->getline();
 }
 print "\n";
