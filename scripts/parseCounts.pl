9a61fd3e52c1181dc659fdb64cd61d080bf20587
cjustin <cjustin@cjustin-G14-laptop>
2021-07-13 05:36:22 -0400

    add basic count to code conversion script

---
+++ scripts/parseCounts.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+#given reference genomeID and file -> extract from master bed file
+
+use warnings;
+use strict;
+use diagnostics;
+use IO::File;
+
+my $fh   = new IO::File( $ARGV[0], "r" );
+my $line = $fh->getline();
+
+my $threshold = 3;
+
+while ($line) {
+	chomp($line);
+	my @tempArr = split( /\t/, $line );
+	
+	if($tempArr[0] > $threshold && $tempArr[2] > $threshold){
+		print "3";
+	}
+	elsif($tempArr[0] > $threshold){
+		print "1";
+	}
+	elsif($tempArr[1] > $threshold){
+		print "2";
+	}
+	else{
+		print "0";
+	}
+	print "\n";
+
+	$line = $fh->getline();
+}
+$fh->close();
