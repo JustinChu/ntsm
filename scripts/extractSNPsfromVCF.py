63fcdc0fd28275d109c45ef7d00661966df90f10
cjustin <cjustin@cjustin-G14-laptop>
2022-01-28 15:03:30 -0500

    allow for vcf sites without IDs

--- scripts/extractSNPsfromVCF.py
+++ scripts/extractSNPsfromVCF.py
@@ -44,12 +44,17 @@ class ExtractKmers:
         #chr1    45508256    rs2275276    A    G    .    .    .  
         lines = vcfFH.readlines()
         # Strips the newline character
+        idCounter = 0
         for line in lines:
             if line[0] != "#":
                 tmpArr = line.rstrip().split("\t")
+                snpID = tmpArr[2]
+                if snpID == '.':
+                    snpID = idCounter
+                    idCounter += 1
                 #store chr, pos, variant
                 info = VCFEntry(tmpArr[0], tmpArr[1], tmpArr[3], tmpArr[4])
-                self._vcfEntries[tmpArr[2]] = info
+                self._vcfEntries[snpID] = info
             
     def extract(self):
         self._parseVCF()
