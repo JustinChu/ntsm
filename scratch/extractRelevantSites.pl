11277e0444cb1c947824693d7ee86c84d1b1735d
JustinChu <cjustin@ds.dfci.harvard.edu>
2022-12-10 02:49:35 -0800

    bugfix

--- scratch/extractRelevantSites.pl
+++ scratch/extractRelevantSites.pl
@@ -8,7 +8,7 @@ use IO::File;
 my %chrPosToRSID;
 
 #vcf file with IDs to filter main list with
-my $fh   = new IO::File( $ARGV[0], "r" );
+my $fh = new IO::File( $ARGV[0], "r" );
 my $line = $fh->getline();
 
 while ($line) {
@@ -17,7 +17,10 @@ while ($line) {
 
 		#chr1	918870	rs7537756	A	G	.	.
 		my @tempArr = split( /\s/, $line );
-		$chrPosToRSID{ $tempArr[0] . "\t" . $tempArr[1] . "\t" . $tempArr[3] . "\t" . $tempArr[4]} = $tempArr[2];
+		$chrPosToRSID{ $tempArr[0] . "\t"
+			  . $tempArr[1] . "\t"
+			  . $tempArr[3] . "\t"
+			  . $tempArr[4] } = $tempArr[2];
 	}
 	$line = $fh->getline();
 }
@@ -26,22 +29,35 @@ $fh->close();
 #main list VCF
 my $fh2 = new IO::File( $ARGV[1], "r" );
 $line = $fh2->getline();
-my $count = 0;
+my $headerStr = "";
+
+
 
 while ($line) {
 	if ($line) {
 		chomp($line);
-
-		#chr1	911428	1:911428:C:T	C	T
-		my @tempArr = split( /\t/, $line );
-		my $id      = $tempArr[0] . "\t" . $tempArr[1] . "\t" . $tempArr[3] . "\t" . $tempArr[4];
-		if ( exists( $chrPosToRSID{$id} )) {
-			$tempArr[2] = $chrPosToRSID{$id};
-			print $tempArr[0];
-			for(my $i = 1; $i < scalar(@tempArr) ; ++$i){
-				print "\t" . $tempArr[$i];
+		if ( $line !~ /^#/ ) {
+			#chr1	911428	1:911428:C:T	C	T
+			my @tempArr = split( /\t/, $line );
+			my $id =
+			    $tempArr[0] . "\t"
+			  . $tempArr[1] . "\t"
+			  . $tempArr[3] . "\t"
+			  . $tempArr[4];
+			if ( exists( $chrPosToRSID{$id} ) ) {
+				$tempArr[2] = $chrPosToRSID{$id};
+				print $tempArr[0];
+				for ( my $i = 1 ; $i < scalar(@tempArr) ; ++$i ) {
+					print "\t" . $tempArr[$i];
+				}
+				print "\n";
 			}
-			print "\n";
+		}
+		elsif($headerStr eq ""){
+			if($line =~ /^#CHROM/){
+				$headerStr = $line;
+				print $line ."\n";
+			}
 		}
 	}
 	$line = $fh2->getline();
