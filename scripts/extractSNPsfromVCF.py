232e748c4f21cea70c8f23d5b23c25ce03c287e0
cjustin <cjustin@bcgsc.ca>
2024-11-14 18:44:22 -0800

    Automatically remove duplicate k-mers during site extraction

--- scripts/extractSNPsfromVCF.py
+++ scripts/extractSNPsfromVCF.py
@@ -20,8 +20,36 @@ class ExtractKmers:
         self._ignore = ignore
         self._subKmer = subKmer
         self._vcfEntries = {}
+        self._kmers = {}
+        
+    def _encodeKmer(self, kmer):    
+        fw_encode = {'A': 0, 'T': 1, 'C': 2, 'G': 3}
+        rv_encode = {'A': 1, 'T': 0, 'C': 3, 'G': 2}
+        fw = 0
+        rv = 0
+        
+        for base in kmer:
+            fw = (fw << 2) | fw_encode[base]
+        
+        for base in reversed(kmer):
+            rv = (rv << 2) | rv_encode[base]
+        
+        if(fw < rv):
+            return fw
+        else:
+            return rv
+        
+    def _insertKmer(self, kmerStr):
+        kmer = self._encodeKmer(kmerStr)
+        if kmer in self._kmers:
+            self._kmers[kmer] += 1
+        else:
+            self._kmers[kmer] = 1
     
-    #Returns True if bases are not purine to pyrimidine
+    def _getKmerCount(self, kmerStr):
+        kmer = self._encodeKmer(kmerStr)
+        return self._kmers[kmer]
+    
     def _checkVariant(self, base1, base2):
         if(base1 == "A" or base1 == "T"):
             if(base2 == "A" or base2 == "T"):
@@ -66,12 +94,36 @@ class ExtractKmers:
         self._parseVCF()
         #open fasta file
         fastaFile = Fasta(self._fasta)
-        # refFH = open(self._prefix + "_AT.fa", 'w')
-        # varFH = open(self._prefix + "_CG.fa", 'w')
+        
+        for id in self._vcfEntries.keys():
+            # print string to repective files
+            chr = self._vcfEntries[id].chr
+            offset = self._vcfEntries[id].pos - 1
+            pos1 = math.ceil(offset - self._k / 2)
+            pos2 = pos1 + self._k
+            tmpStr = str(fastaFile[self._vcfEntries[id].chr][pos1:pos2])
+            tmpStr = tmpStr.upper()
+            if self._vcfEntries[id].wt != tmpStr[int(self._k / 2)]:
+                print("Wildtype allele does not match", file=sys.stderr)
+                print("ref:" + self._vcfEntries[id].wt, file=sys.stderr)
+                print("var:" + self._vcfEntries[id].variant, file=sys.stderr)
+                print("fasta:" + str(fastaFile[self._vcfEntries[id].chr][offset]), file=sys.stderr)
+                print("kmer:" + tmpStr, file=sys.stderr)
+                continue
+            
+            if self._checkVariant(self._vcfEntries[id].wt, self._vcfEntries[id].variant) and self._ignore:
+                continue
+            
+            modStr = tmpStr[0:int(self._k / 2)] + self._vcfEntries[id].variant + tmpStr[int(self._k / 2) + 1:]
+            #count string
+            for pos in range(0, len(modStr) - self._subKmer + 1):
+                self._insertKmer(tmpStr[pos:pos + self._subKmer])
+                self._insertKmer(modStr[pos:pos + self._subKmer])
         
         removeCount = 0
         processCount = 0
         atToCGFilterCount = 0
+        kmersRemoved = 0
         #for each vcf entry extract wildtype and variant into a string
         for id in self._vcfEntries.keys():
             # print string to repective files
@@ -81,49 +133,55 @@ class ExtractKmers:
             pos2 = pos1 + self._k
             tmpStr = str(fastaFile[self._vcfEntries[id].chr][pos1:pos2])
             tmpStr = tmpStr.upper()
-            if(self._vcfEntries[id].wt != tmpStr[int(self._k / 2)]):
-                print("Wildtype allele does not match", file=sys.stderr)
-                print("ref:" + self._vcfEntries[id].wt, file=sys.stderr)
-                print("var:" + self._vcfEntries[id].variant, file=sys.stderr)
-                print("fasta:" + str(fastaFile[self._vcfEntries[id].chr][offset]), file=sys.stderr)
-                print("kmer:" + tmpStr, file=sys.stderr)
+            if self._vcfEntries[id].wt != tmpStr[int(self._k / 2)]:
                 removeCount += 1
                 continue
             
-            if(self._checkVariant(self._vcfEntries[id].wt, self._vcfEntries[id].variant)):
-                # print("Warning: " + id +" " +self._vcfEntries[id].variant + " " + self._vcfEntries[id].wt 
-                #       + " is not a A/T <-> C/G variant.", file=sys.stderr)
+            if self._checkVariant(self._vcfEntries[id].wt, self._vcfEntries[id].variant) and self._ignore:
                 removeCount += 1
                 atToCGFilterCount += 1
-                if(self._ignore):
-                    continue
+                continue
+            
             modStr = tmpStr[0:int(self._k / 2)] + self._vcfEntries[id].variant + tmpStr[int(self._k / 2) + 1:]
-            # varFH.write(">" + id + "\n")
-            # refFH.write(">" + id + "\n")            
-            if(self._orderVariant(self._vcfEntries[id].wt, self._vcfEntries[id].variant)):
-                # refFH.write(tmpStr + "\n")
-                # varFH.write(modStr + "\n")
+            retainedKmers = kmersRemoved
+            if self._orderVariant(self._vcfEntries[id].wt, self._vcfEntries[id].variant):
                 #sub k-mer stuff here
-                if self._subKmer != 0:
-                    for pos in range(0, len(modStr) - self._subKmer + 1):
+                for pos in range(0, len(modStr) - self._subKmer + 1):
+                    modKmer = modStr[pos:pos + self._subKmer]
+                    kmer = tmpStr[pos:pos + self._subKmer]
+                    
+                    if self._getKmerCount(kmer) == 1:
                         print(">" + id + "|" + str(pos) + "|AT")
                         print(tmpStr[pos:pos + self._subKmer])
+                    else:
+                        kmersRemoved += 1
+                        
+                    if self._getKmerCount(modKmer) == 1:
                         print(">" + id + "|" + str(pos) + "|CG")
                         print(modStr[pos:pos + self._subKmer])
+                    else:
+                        kmersRemoved += 1
             else:
-                # refFH.write(modStr + "\n")
-                # varFH.write(tmpStr + "\n")
                 #sub k-mer stuff here
-                if self._subKmer != 0:
-                    for pos in range(0, len(modStr) - self._subKmer + 1):
+                for pos in range(0, len(modStr) - self._subKmer + 1):
+                    modKmer = modStr[pos:pos + self._subKmer]
+                    kmer = tmpStr[pos:pos + self._subKmer]
+                    if self._getKmerCount(modKmer) == 1:
                         print(">" + id + "|" + str(pos) + "|AT")
                         print(modStr[pos:pos + self._subKmer])
+                    else:
+                        kmersRemoved += 1
+                    if self._getKmerCount(kmer) == 1:    
                         print(">" + id + "|" + str(pos) + "|CG")
                         print(tmpStr[pos:pos + self._subKmer])
+                    else:
+                        kmersRemoved += 1
+            if (kmersRemoved - retainedKmers) == (len(modStr) - self._subKmer + 1):
+                removeCount += 1
+            
             processCount += 1
-        # refFH.close()
-        # varFH.close()
-        print("Processed " + str(processCount) + " SNPs. Removed " + str(removeCount) + " SNPs.", file=sys.stderr)
+            
+        print("Processed " + str(processCount) + " SNPs. Removed " + str(removeCount) + " SNPs. " + str(kmersRemoved) + " duplicate k-mers removed.", file=sys.stderr)
         if atToCGFilterCount > 0:
             print("Filtered " + str(atToCGFilterCount) + " SNPs that did not have A/T to C/G variants", file=sys.stderr)
         
@@ -131,15 +189,14 @@ def main():
     parser = argparse.ArgumentParser(description='Extract k-mers listed from VCF file from reference. Produces 2 files (reference and variant)')
     parser.add_argument("-v", '--vcf', type=str, dest='vcf', help='vcf File')
     parser.add_argument("-f", '--fa', type=str, dest='fasta', help='fasta file with fai index')
-    parser.add_argument("-k", '--kmer', type=int, dest='kmer', help='kmer size', default=25)
+    parser.add_argument("-k", '--kmer', type=int, dest='kmer', help='kmer size', default=31)
     parser.add_argument("-p", '--prefix', type=str, dest='prefix', help='output prefix', default = "")
     parser.add_argument("-i", '--ignoreReq', action='store_false', dest='ignore', help='ignore AT to CG conversion requirements', default = True)
-    parser.add_argument("-s", '--subKmer', type=int, dest='subKmer', help='Sub kmer size, only needed kmer overlap integrity purposes', default=0)
+    parser.add_argument("-s", '--subKmer', type=int, dest='subKmer', help='Sub kmer size, only needed for kmer overlap integrity purposes', default=19)
 
     args = parser.parse_args()
     extractor = ExtractKmers(args.vcf, args.fasta, args.kmer, args.prefix, args.ignore, args.subKmer)
     extractor.extract()
     
-
 main()
     
--- scripts/filterRepetiveSNP.pl
+++ scripts/filterRepetiveSNP.pl
@@ -4,60 +4,73 @@ use strict;
 use diagnostics;
 use IO::File;
 
-my $fh     = new IO::File( $ARGV[0], "r" );
-my $prefix = $ARGV[1];
-my $line   = $fh->getline();
+my $fh       = new IO::File( $ARGV[0], "r" );
+my $prefix   = $ARGV[1];
+my $maxCount = $ARGV[2] - $ARGV[3] + 1;
+my $line     = $fh->getline();
+chomp($line);
 
 #id->type(AT,CG)->count
 my %idToUniqCount;
 my %idToStr;
 
-my $maxCount = 1;
+#my %currPos = (
+#	"AT" => 0,
+#	"CG" => 0
+#);
 
 while ($line) {
 	chomp($line);
-	if ($line) {
 
 #rs4970383|0|AT	0	chr1	903160	20	19M	*	0	0	CCCATGACCCCATTTCACC	*	XT:A:U	NM:i:0	X0:i:1	X1:i:2	XM:i:0	XO:i:0	XG:i:0	MD:Z:19	XA:Z:chr20,+44466325,19M,1;chr15,+45228912,19M,1;
-		my @tmpAr = split( /\t/, $line );
-		if ( $tmpAr[0] =~ /([^\|]+)\|\d+\|(AT|CG)/ ) {
-			my $id   = $1;
-			my $type = $2;
-			my $seq  = $tmpAr[9];
-			if ( !exists( $idToUniqCount{$id}{$type} ) ) {
-				$idToUniqCount{$id}{$type} = 0;
+	my @tmpAr = split( /\t/, $line );
+	if ( $tmpAr[0] =~ /([^\|]+)\|(\d+)\|(AT|CG)/ ) {
+		my $id   = $1;
+		my $pos  = $2;
+		my $type = $3;
+		my $seq  = $tmpAr[9];
+		if ( !exists( $idToUniqCount{$id}{$type} ) ) {
+			$idToUniqCount{$id}{$type} = $maxCount;
+		}
+		if ( $line =~ /X0:i:(\d+)/ ) {
+			my $count = $1;
+			if ( $line =~ /.*X1:i:(\d+)/ ) {
+				$count += $1;
 			}
-			if ( $line =~ /X0:i:(\d+)/ ) {
-				my $count = $1;
-				if ( $line =~ /.*X1:i:(\d+)/ ) {
-					$count += $1;
-				}
-				if ( $count > 1 ) {
-					if ( $maxCount < ++$idToUniqCount{$id}{$type} ) {
-						$maxCount = $idToUniqCount{$id}{$type};
-					}
-				}
-				else {
-					if ( exists( $idToStr{$id}{$type} ) ) {
-						$idToStr{$id}{$type} .= 'N' . $seq;
-					}
-					else {
-						$idToStr{$id}{$type} = $seq;
-					}
-				}
-			}
-			else {
+			if ( $count > 1 ) {
 				if ( exists( $idToStr{$id}{$type} ) ) {
-					$idToStr{$id}{$type} .= 'N' . $seq;
+#					if ( $currPos{$type} - $pos == 1 ) {
+#						$idToStr{$id}{$type} .= substr( $seq, -1 );
+#					}
+#					else {
+						$idToStr{$id}{$type} .= 'N' . $seq;
+#					}
 				}
 				else {
 					$idToStr{$id}{$type} = $seq;
 				}
+#				$currPos{$type} = $pos;
+				--$idToUniqCount{$id}{$type};
 			}
 		}
 		else {
-			print STDERR "unable to parse: " . $line . "\n";
+			if ( exists( $idToStr{$id}{$type} ) ) {
+#				if ( $currPos{$type} - $pos == 1 ) {
+#					$idToStr{$id}{$type} .= substr( $seq, -1 );
+#				}
+#				else {
+					$idToStr{$id}{$type} .= 'N' . $seq;
+#				}
+			}
+			else {
+				$idToStr{$id}{$type} = $seq;
+			}
+#			$currPos{$type} = $pos;
+			--$idToUniqCount{$id}{$type};
 		}
+	}
+	else {
+		print STDERR "unable to parse: " . $line . "\n";
 	}
 	$line = $fh->getline();
 }
@@ -71,17 +84,29 @@ for ( my $i = 0 ; $i < $maxCount ; ++$i ) {
 
 foreach my $id ( sort keys(%idToUniqCount) ) {
 	for ( my $i = 0 ; $i < $maxCount ; ++$i ) {
-		if (   $idToUniqCount{$id}{"AT"} <= $i
-			&& $idToUniqCount{$id}{"CG"} <= $i )
+		if (   exists( $idToUniqCount{$id}{"AT"} )
+			&& exists( $idToUniqCount{$id}{"CG"} ) )
 		{
-			if (   exists( $idToStr{$id}{"AT"} )
-				&& exists( $idToStr{$id}{"CG"} ) )
+			if (   $idToUniqCount{$id}{"AT"} <= $i
+				&& $idToUniqCount{$id}{"CG"} <= $i )
 			{
-				$fhs[$i]->write( ">" . $id . " ref\n" . $idToStr{$id}{"AT"} . "\n" );
-				$fhs[$i]->write( ">" . $id . " var\n" . $idToStr{$id}{"CG"} . "\n" );
-			}
-			else {
-				print STDERR $id . " " . $i . "\n";
+				if (   exists( $idToStr{$id}{"AT"} )
+					&& exists( $idToStr{$id}{"CG"} ) )
+				{
+					$fhs[$i]->write(
+						">" . $id . " ref\n" . $idToStr{$id}{"AT"} . "\n" );
+					$fhs[$i]->write(
+						">" . $id . " var\n" . $idToStr{$id}{"CG"} . "\n" );
+				}
+				else {
+					print STDERR "Possible file truncation. Missing: "
+					  . $id . " "
+					  . $i . " "
+					  . $idToUniqCount{$id}{"AT"} . " "
+					  . $idToUniqCount{$id}{"CG"} . " "
+					  . int( exists( $idToStr{$id}{"AT"} ) ) . " "
+					  . int( exists( $idToStr{$id}{"CG"} ) ) . "\n";
+				}
 			}
 		}
 	}
